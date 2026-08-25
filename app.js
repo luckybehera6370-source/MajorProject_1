3c0103fd33fc9150bfb70e8fe3a1580fb7db671f
Lucky Behera <luckybehera6370@gmail.com>
2026-08-25 16:29:53 +0530

    Adding some Updates

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema ,reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
@@ -49,6 +49,16 @@ app.get("/" , (req,res) =>{
 
 const validateListing = (req,res,next) => {
   let {error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map(el => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }else{
+    next();
+  }
+};
+
+const validateReview = (req,res,next) => {
+  let {error} = reviewSchema.validate(req.body);
   if(error){
     let errMsg = error.details.map(el => el.message).join(",");
     throw new ExpressError(400, errMsg);
@@ -111,7 +121,7 @@ app.get("/listings/:id", wrapAsync(async (req,res) =>{
 //Reviews
 //Post Route
 
-app.post("/listings/:id/reviews", wrapAsync(async (req, res) => {
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
     return res.status(404).send("Listing not found");
@@ -142,8 +152,7 @@ app.post("/listings/:id/reviews", wrapAsync(async (req, res) => {
 
 app.use((err,req,res,next) => {
     let {statusCode = 500,message = "Something went wrong"} = err;
-    res.render("error",{err});
-    res.status(statusCode).send(message);
+  return res.status(statusCode).render("error",{err, message});
 });
 
 app.listen(8080, () =>{
