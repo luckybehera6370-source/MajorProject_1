e26a1faf86bc68dd17aacc0e21f3fa84cfa89a2a
Lucky Behera <luckybehera6370@gmail.com>
2026-09-24 12:18:08 +0530

    listing error

--- app.js
+++ app.js
@@ -173,11 +173,11 @@ passport.deserializeUser(
 
 app.use((req, res, next) => {
 
-    res.locals.success = req.flash("success");
+    res.locals.success = req.flash("success") || [];
 
-    res.locals.error = req.flash("error");
+    res.locals.error = req.flash("error") || [];
 
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;
 
     next();
 });
